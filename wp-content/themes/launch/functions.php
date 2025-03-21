6ce6ca79850b1ab7b2fcea1ea9b972b92d4f0803
Rubel khan <rubel.pust53@gmail.com>
2025-03-21 15:24:37 +0600

    added launch assets and launch theme setup

---
+++ wp-content/themes/launch/functions.php
@@ -0,0 +1,42 @@
+<?php
+
+function launch_theme_setup(){
+    load_theme_textdomain("launch");
+    add_theme_support("post-thumbnails");
+    add_theme_support("title-tag");
+
+
+
+}
+add_action("after_setup_thems","launch_theme_setup");
+
+
+function launch_assets(){
+
+    wp_enqueue_style("animate-css",get_template_directory_uri()."/assets/css/animate.css");
+    wp_enqueue_style("icomoon-css", get_template_directory_uri()."/assets/css/icomoon.css");
+    wp_enqueue_style("bootstrap-css", get_template_directory_uri()."/assets/css/bootstrap.css");
+    wp_enqueue_style("style-css", get_template_directory_uri()."/assets/css/style.css");
+    wp_enqueue_style("launcher", get_stylesheet_uri());
+
+
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script("easing-jquery", get_template_directory_uri()."/assets/js/jquery.easing.1.3.js", array("jquery"), null, true);
+    wp_enqueue_script("bootstrap-min-js", get_template_directory_uri()."/assets/js/bootstrap.min.js", array("jquery"), null, true);
+    wp_enqueue_script("waypoints-min-js", get_template_directory_uri()."/assets/js/jquery.waypoints.min.js", array("jquery"), null, true);
+    wp_enqueue_script("simply-countdown", get_template_directory_uri()."/assets/js/simplyCountdown.js", array("jquery"), null, true);
+    wp_enqueue_script("main-js", get_template_directory_uri()."/assets/js/main.js", array("jquery"), null,true);
+    wp_enqueue_script("ejquery.min", get_template_directory_uri()."/assets/js/jquery.min.js", array("jquery"), null, true);
+    wp_enqueue_script("jquery-stellar", get_template_directory_uri()."/assets/js/jquery.stellar.min.js", array("jquery"), null, true);
+
+
+}
+add_action("wp_enqueue_scripts", "launch_assets");
+
+
+
+
+
+
+?>
--- wp-content/themes/launch/lunch.php
+++ wp-content/themes/launch/lunch.php
@@ -5,3 +5,111 @@ Template Name: Launcher Homepage
 */
 
 ?>
+
+<!DOCTYPE html>
+	<head>
+	<meta charset="utf-8">
+	
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+
+	</head>
+	<body>
+		
+	<div class="fh5co-loader"></div>
+	
+	<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 text-center">
+					<div id="fh5co-logo"><a href="index.html">Launch<strong>.</strong></a></div>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	<header id="fh5co-header" class="fh5co-cover" role="banner" style="background-image:url(images/img_bg_1.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>We Are Coming Very Soon!</h1>
+							<h2>Free html5 templates Made by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+							<div class="simply-countdown simply-countdown-one"></div>
+							<div class="row">
+								<h2>Notify me when it's ready</h2>
+								<form class="form-inline" id="fh5co-header-subscribe">
+									<div class="col-md-12 col-md-offset-0">
+										<div class="form-group">
+											<input type="text" class="form-control" id="email" placeholder="Get notify by email">
+											<button type="submit" class="btn btn-primary">Send</button>
+										</div>
+									</div>
+								</form>
+								<ul class="fh5co-social-icons">
+									<li><a href="#"><i class="icon-twitter-with-circle"></i></a></li>
+									<li><a href="#"><i class="icon-facebook-with-circle"></i></a></li>
+									<li><a href="#"><i class="icon-linkedin-with-circle"></i></a></li>
+									<li><a href="#"><i class="icon-dribbble-with-circle"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FREEHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<script>
+    var d = new Date(new Date().getTime() + 800 * 120 * 120 * 2000);
+
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+    });
+
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: false
+    });
+</script>
+
+	</body>
+</html>
+
